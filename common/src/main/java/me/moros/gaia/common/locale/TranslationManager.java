868749d3b97a9a6a98cd8bef01a821001b2e1056
PrimordialMoros <mail@moros.me>
2023-07-10 23:17:33 +0300

    Add watch service delay

--- common/src/main/java/me/moros/gaia/common/locale/TranslationManager.java
+++ common/src/main/java/me/moros/gaia/common/locale/TranslationManager.java
@@ -25,13 +25,12 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
+import java.util.List;
 import java.util.Locale;
 import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -46,71 +45,71 @@ import org.spongepowered.configurate.reference.WatchServiceListener;
 public class TranslationManager {
   private static final String PATH = "gaia.lang.messages_en";
   private static final Locale DEFAULT_LOCALE = Locale.ENGLISH;
-  private static final Object lock = new Object();
+  private static final long DELAY = 500;
 
   private final Logger logger;
   private final Path translationsDirectory;
+  private final AtomicLong lastUpdate;
+  private final AtomicReference<ForwardingTranslationRegistry> registryReference;
   private final WatchServiceListener listener;
-  private final Set<Locale> installed;
-  private TranslationRegistry registry;
 
   public TranslationManager(Logger logger, Path directory) {
     this.logger = logger;
-    this.installed = ConcurrentHashMap.newKeySet();
+    this.lastUpdate = new AtomicLong();
     try {
       this.translationsDirectory = Files.createDirectories(directory.resolve("translations"));
+      var registry = createRegistry();
+      this.registryReference = new AtomicReference<>(registry);
+      GlobalTranslator.translator().addSource(registry);
       this.listener = WatchServiceListener.create();
       this.listener.listenToDirectory(translationsDirectory, e -> reload());
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
-    reload();
   }
 
   private void reload() {
-    var newRegistry = TranslationRegistry.create(Key.key("gaia", "translations"));
-    newRegistry.defaultLocale(DEFAULT_LOCALE);
-    var wrappedRegistry = loadCustom(logger, translationsDirectory, new Registry(newRegistry, new HashSet<>()));
-    ResourceBundle bundle = ResourceBundle.getBundle(PATH, DEFAULT_LOCALE, UTF8ResourceBundleControl.get());
-    wrappedRegistry.registry().registerAll(DEFAULT_LOCALE, bundle, false);
-
-    synchronized (lock) {
-      if (registry != null) {
-        GlobalTranslator.translator().removeSource(registry);
-      }
-      registry = wrappedRegistry.registry();
-      GlobalTranslator.translator().addSource(registry);
-      installed.clear();
-      installed.addAll(wrappedRegistry.locales());
+    long time = System.currentTimeMillis();
+    long previous = lastUpdate.getAndSet(time);
+    if (time < previous + DELAY) {
+      return;
     }
-  }
-
-  public void close() {
-    try {
-      listener.close();
-    } catch (IOException e) {
-      logger.warn(e.getMessage(), e);
-    }
-  }
-
-  private static Registry loadCustom(Logger logger, Path dir, Registry wrappedRegistry) {
-    Collection<Path> files;
-    try (Stream<Path> stream = Files.list(dir)) {
-      files = stream.filter(TranslationManager::isValidPropertyFile).collect(Collectors.toList());
-    } catch (IOException e) {
-      files = Collections.emptyList();
-    }
-    files.forEach(path -> loadTranslationFile(logger, wrappedRegistry, path));
-    int amount = wrappedRegistry.locales().size();
+    var newRegistry = createRegistry();
+    var old = registryReference.getAndSet(newRegistry);
+    GlobalTranslator.translator().removeSource(old);
+    GlobalTranslator.translator().addSource(newRegistry);
+    int amount = newRegistry.locales().size();
     if (amount > 0) {
-      String translations = wrappedRegistry.locales().stream().map(Locale::getLanguage)
+      String translations = newRegistry.locales().stream().map(Locale::getLanguage)
         .collect(Collectors.joining(", ", "[", "]"));
       logger.info(String.format("Loaded %d translations: %s", amount, translations));
     }
-    return wrappedRegistry;
   }
 
-  private static void loadTranslationFile(Logger logger, Registry wrappedRegistry, Path path) {
+  private ForwardingTranslationRegistry createRegistry() {
+    var registry = new ForwardingTranslationRegistry(Key.key("gaia", "translations"));
+    registry.defaultLocale(DEFAULT_LOCALE);
+    loadCustom(registry);
+    loadDefaults(registry);
+    return registry;
+  }
+
+  private void loadDefaults(TranslationRegistry registry) {
+    ResourceBundle bundle = ResourceBundle.getBundle(PATH, DEFAULT_LOCALE, UTF8ResourceBundleControl.get());
+    registry.registerAll(DEFAULT_LOCALE, bundle, false);
+  }
+
+  private void loadCustom(TranslationRegistry registry) {
+    Collection<Path> files;
+    try (Stream<Path> stream = Files.list(translationsDirectory)) {
+      files = stream.filter(this::isValidPropertyFile).toList();
+    } catch (IOException e) {
+      files = List.of();
+    }
+    files.forEach(f -> loadTranslationFile(f, registry));
+  }
+
+  private void loadTranslationFile(Path path, TranslationRegistry registry) {
     String localeString = removeFileExtension(path);
     Locale locale = Translator.parseLocale(localeString);
     if (locale == null) {
@@ -124,19 +123,23 @@ public class TranslationManager {
       logger.warn("Error loading locale file: " + localeString);
       return;
     }
-    wrappedRegistry.registry().registerAll(locale, bundle, false);
-    wrappedRegistry.locales().add(locale);
+    registry.registerAll(locale, bundle, false);
   }
 
-  private static boolean isValidPropertyFile(Path path) {
+  private boolean isValidPropertyFile(Path path) {
     return Files.isRegularFile(path) && path.getFileName().toString().endsWith(".properties");
   }
 
-  private static String removeFileExtension(Path path) {
+  private String removeFileExtension(Path path) {
     String fileName = path.getFileName().toString();
     return fileName.substring(0, fileName.length() - ".properties".length());
   }
 
-  private record Registry(TranslationRegistry registry, Set<Locale> locales) {
+  public void close() {
+    try {
+      listener.close();
+    } catch (IOException e) {
+      logger.warn(e.getMessage(), e);
+    }
   }
 }
