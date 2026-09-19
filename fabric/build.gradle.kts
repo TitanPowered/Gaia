0c67c8155fbb3d3e8c5851ba48f04316e7245a38
PrimordialMoros <mail@moros.me>
2026-09-19 22:38:01 +0300

    bump dependencies for mc 26.3 and define env in minotaur for modrinth publishing

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -60,6 +60,7 @@ gaiaPlatform {
 modrinth {
     versionName = "fabric-$version"
     gameVersions.add(libs.versions.minecraft)
+    environment = "dedicated_server_only"
     dependencies {
         required.project("fabric-api")
     }
